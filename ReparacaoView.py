f59c9f276956d96ccf72da316e644b9c27e4be41
felix mavila <felixmacarenco@gmail.com>
2024-05-27 16:13:27 +0100

    feat: create reparacao feature Crud

--- ReparacaoView.py
+++ ReparacaoView.py
@@ -7,34 +7,32 @@ from tkinter import messagebox
 # Conectando ao banco de dados
 conexao = lite.connect('Oficina.db')
 
-# Criando a tabela tbl_viaturas se não existir
+# Criando a tabela tbl_reparacao se não existir
 with conexao:
     cur = conexao.cursor()
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS tbl_viaturas (
-            id_viatura INTEGER PRIMARY KEY AUTOINCREMENT,
-            modelo TEXT,
-            marca TEXT,
-            id_cliente INTEGER,
-            matricula TEXT,
-            cor TEXT,
-            kms DOUBLE
+        CREATE TABLE IF NOT EXISTS tbl_reparacao (
+            id_reparacao INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_viatura INTEGER,
+            data TEXT,
+            kms DOUBLE,
+            descricao TEXT
         )
     """)
 
-# Função para inserir dados na tabela tbl_viaturas
+# Função para inserir dados na tabela tbl_reparacao
 def inserir_dados(i):
     with conexao:
         cur = conexao.cursor()
-        query = "INSERT INTO tbl_viaturas (modelo, marca, id_cliente, matricula, cor, kms) VALUES (?,?,?,?,?,?)"
+        query = "INSERT INTO tbl_reparacao (id_viatura, data, kms, descricao) VALUES (?,?,?,?)"
         cur.execute(query, i)
 
-# Função para obter informações da tabela tbl_viaturas
+# Função para obter informações da tabela tbl_reparacao
 def mostrar_info():
     lista = []
     with conexao:
         cur = conexao.cursor()
-        query = "SELECT * FROM tbl_viaturas"
+        query = "SELECT * FROM tbl_reparacao"
         cur.execute(query)
         informacoes = cur.fetchall()
         for i in informacoes:
@@ -43,26 +41,22 @@ def mostrar_info():
 
 # Função para inserir dados através da interface gráfica
 def inserir():
-    modelo = caixaTexto_modelo.get()
-    marca = caixa_marca.get()
-    id_cliente = caixa_id_Cliente.get()
-    matricula = caixa_matricula.get()
-    cor = caixa_cor.get()
+    id_viatura = caixaTexto_id_viatura.get()
+    data = caixa_data.get()
     kms = caixa_kms.get()
+    descricao = caixa_descricao.get()
     
-    lista = [modelo, marca, id_cliente, matricula, cor ,kms]
+    lista = [id_viatura, data, kms, descricao]
 
-    if not modelo or not marca or not id_cliente or not matricula or not cor or not kms:
+    if not id_viatura or not data or not kms or not descricao:
         messagebox.showerror('Erro', 'Todos os campos são obrigatórios')
     else:
         inserir_dados(lista)
         messagebox.showinfo('Sucesso', 'Os dados foram inseridos com sucesso')
-        caixaTexto_modelo.delete(0, END)
-        caixa_marca.delete(0, END)
-        caixa_id_Cliente.delete(0, END)
-        caixa_matricula.delete(0, END)
-        caixa_cor.delete(0, END)
-        caixa_kms.delete(0, END)
+        caixaTexto_id_viatura.delete(0,'end')
+        caixa_data.delete(0,'end')
+        caixa_kms.delete(0,'end')
+        caixa_descricao.delete(0,'end')
 
         mostrar()
 
@@ -83,8 +77,8 @@ def mostrar():
 
     frame_centro.grid_rowconfigure(0, weight=12)
 
-    hd = ["nw", "nw", "nw", "nw", "nw", "center", "center"]
-    h = [30, 170, 140, 100, 120, 150, 100]
+    hd = ["nw", "nw", "center", "center", "nw"]
+    h = [50, 100, 100, 100, 200]
     n = 0
 
     for col in tabela_head:
@@ -112,7 +106,7 @@ co9 = "#e9edf5"  # Sky Blue
 
 # Criando janela principal
 janela = Tk()
-janela.title("Formulário de Clientes")
+janela.title("Formulário de Reparação")
 janela.geometry("1043x453")
 janela.configure(background=co9)
 janela.resizable(width=False, height=False)
@@ -128,45 +122,33 @@ frame_centro = Frame(janela, width=588, height=403, bg=co1, relief='flat')
 frame_centro.grid(row=0, column=1, rowspan=2, padx=1, pady=0, sticky='NSEW')
 
 # Label de título
-label_titulo = Label(frame_cima, text='Formulário de Viaturas', anchor=NW, font=('Ivy 13 bold'), bg=co2, fg=co1, relief='flat')
+label_titulo = Label(frame_cima, text='Formulário de Reparação', anchor=NW, font=('Ivy 13 bold'), bg=co2, fg=co1, relief='flat')
 label_titulo.place(x=10, y=20)
 
 # Configurando Frame de baixo (campos de entrada)
-label_modelo = Label(frame_baixo, text='Modelo *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
-label_modelo.place(x=10, y=10)
+label_id_viatura = Label(frame_baixo, text='Id_Viatura *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
+label_id_viatura.place(x=10, y=10)
 
-caixaTexto_modelo = Entry(frame_baixo, width=45, justify='left', relief='solid')
-caixaTexto_modelo.place(x=10, y=40)
+caixaTexto_id_viatura = Entry(frame_baixo, width=45, justify='left', relief='solid')
+caixaTexto_id_viatura.place(x=10, y=40)
 
-label_marca = Label(frame_baixo, text='Marca *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
-label_marca.place(x=10, y=70)
+label_data = Label(frame_baixo, text='Data *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
+label_data.place(x=10, y=70)
 
-caixa_marca = Entry(frame_baixo, width=45, justify='left', relief='solid')
-caixa_marca.place(x=10, y=100)
-
-label_id_cliente = Label(frame_baixo, text='Id_Cliente *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
-label_id_cliente.place(x=10, y=130)
-
-caixa_id_Cliente = Entry(frame_baixo, width=45, justify='left', relief='solid')
-caixa_id_Cliente.place(x=10, y=160)
-
-label_matricula = Label(frame_baixo, text='Matricula *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
-label_matricula.place(x=10, y=190)
-
-caixa_matricula = Entry(frame_baixo, width=45, justify='left', relief='solid')
-caixa_matricula.place(x=10, y=220)
-
-label_cor = Label(frame_baixo, text='Cor *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
-label_cor.place(x=10, y=250)
-
-caixa_cor = Entry(frame_baixo, width=45, justify='left', relief='solid')
-caixa_cor.place(x=10, y=280)
+caixa_data = Entry(frame_baixo, width=45, justify='left', relief='solid')
+caixa_data.place(x=10, y=100)
 
 label_kms = Label(frame_baixo, text='Kms *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
-label_kms.place(x=10, y=310)
+label_kms.place(x=10, y=130)
 
 caixa_kms = Entry(frame_baixo, width=45, justify='left', relief='solid')
-caixa_kms.place(x=10, y=340)
+caixa_kms.place(x=10, y=160)
+
+label_descricao = Label(frame_baixo, text='Descrição *', anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4, relief='flat')
+label_descricao.place(x=10, y=190)
+
+caixa_descricao = Entry(frame_baixo, width=45, justify='left', relief='solid')
+caixa_descricao.place(x=10, y=220)
 
 # Botões
 botao_inserir = Button(frame_baixo, text="Inserir", command=inserir, bg=co6, fg=co1, font=('Ivy 10 bold'), relief='flat')
@@ -179,7 +161,7 @@ botao_eliminar = Button(frame_baixo, text="Eliminar", bg=co7, fg=co1, font=('Ivy
 botao_eliminar.place(x=210, y=380)
 
 # Configurando Frame do centro (tabela)
-tabela_head = ['ID', 'ID_Cliente', 'Marca', 'Modelo', 'Matricula', 'kms', 'Cor']
+tabela_head = ['ID', 'ID_Viatura', 'Data', 'Kms', 'Descrição']
 
 mostrar()
 
